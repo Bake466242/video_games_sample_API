b42d49f1e95ed11a1abdecd282282e578c10b200
Rebecca Bakels <rebeccabakels@gmail.com>
2021-01-31 11:33:28 -0500

    attempted new .find

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const fetch = require("node-fetch");
 const baseURL = 'https://api.sampleapis.com/switch/games';
 fetch(baseURL)
   .then(resp => resp.json())
-  .then(data => displayData(data));
+  .then(data => displayData(data))
+  .then(data => getName(data))
 
 
 function displayData(data) {
-    let filter = data.map(filter => filter.name)
+    let filter = data.map(filter => filter.name)[31]
     console.log(filter)
 }
+
+function getName(data) {
+  let firName = data.find(firName => firName.id == 1)
+  console.log(firName)
+}
+
+
 
 // let gameName = gameName.filter(game => {
 //     return game.name.includes('Racing')
